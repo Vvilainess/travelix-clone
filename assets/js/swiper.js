9585b932652c0d93adbdc1a53380f07ae49eed3a
Phùng Trường Giang <phungtruonggiang2507@gmail.com>
2022-01-13 15:49:03 +0700

    edit testmonials section and add trending section

--- assets/js/swiper.js
+++ assets/js/swiper.js
@@ -1,10 +1,30 @@
-const swiper = new Swiper(".swiper", {
-    // Optional parameters
+const heroSlider = new Swiper(".swiper.hero__slider", {
     direction: "horizontal",
     loop: true,
     speed: 1000,
     navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
+        nextEl: ".hero-button__slider.swiper-button-next",
+        prevEl: ".hero-button__slider.swiper-button-prev",
+    },
+});
+const ctaSlider = new Swiper(".swiper.cta__slider", {
+    direction: "horizontal",
+    loop: true,
+    speed: 1000,
+    navigation: {
+        nextEl: ".cta-button__slider.swiper-button-next",
+        prevEl: ".cta-button__slider.swiper-button-prev",
+    },
+});
+const testimonialsSlider = new Swiper(".swiper.testimonials__slider", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    slidesPerGroup: 1,
+    loop: true,
+    speed: 800,
+    loopFillGroupWithBlank: true,
+    navigation: {
+        nextEl: ".testimonials-button-slider.swiper-button-next",
+        prevEl: ".testimonials-button-slider.swiper-button-prev",
     },
 });
